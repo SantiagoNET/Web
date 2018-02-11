733fe3473966024b4efd31381e4c060372359c41
Manuel <manuel.simon.novoa@gmail.com>
2018-02-11 03:10:44 +0100

    Listo para lanzar a producción

--- JS/multilang.js
+++ JS/multilang.js
@@ -1,13 +1,13 @@
 // MultiLang - BdR 2016
 // JavaScript object to handle multilanguage, load phrases from JSON etc.
 
-var MultiLang = function (url, lang, onload) {
+var MultiLang = function(url, lang, onload)
+{
     // variables
     this.phrases = {};
 
     // keep only first two chareacters, for example 'en-US', 'fr', 'nl-NL', 'it', 'zh' etc.
-    this.selectedLanguage = (lang || navigator.language || navigator.userLanguage).substring(0, 2);
-    ;
+    this.selectedLanguage = (lang || navigator.language || navigator.userLanguage).substring(0, 2);;
 
     // onLoad callback function, call after loading JSON
     this.onLoad = onload;
@@ -25,7 +25,7 @@ var MultiLang = function (url, lang, onload) {
             //if (evt.target.readyState == 4)// TESTING allow "Cross origin requests" to load from local harddisk
             {
                 // load translations
-                this.phrases = JSON.parse(evt.target.responseText);
+                this.phrases = JSON.parse( evt.target.responseText );
 
                 // verify that the currently selected language exists in the translations
                 this.setLanguage(this.selectedLanguage);
@@ -35,18 +35,16 @@ var MultiLang = function (url, lang, onload) {
                     this.onLoad();
                 }
 
-            }
-            ;
+            };
         }.bind(obj); // NOTE: bind onreadyfunction to MultiLang instead of XMLHttpRequest, so MultiLang.phrases will be set instead of added to XMLHttpRequest
-        req.addEventListener("error", function (e) {
+        req.addEventListener("error", function(e) {
             console.log('MultiLang.js: Error reading json file.');
         }, false);
 
         req.send(null);
-    }
-    ;
+    };
 
-    this.setLanguage = function (langcode) {
+    this.setLanguage = function(langcode) {
 
         // check if language code exists in translations
         if (!this.phrases.hasOwnProperty(langcode)) {
@@ -58,18 +56,15 @@ var MultiLang = function (url, lang, onload) {
                 if (this.phrases.hasOwnProperty(key)) {
                     langcode = key;
                     break;
-                }
-                ;
-            }
-            ;
-        }
-        ;
+                };
+            };
+        };
 
         // set as selected language code
         this.selectedLanguage = langcode;
     };
 
-    this.get = function (key) {
+    this.get = function(key) {
         // get key phrase
         var str;
 
